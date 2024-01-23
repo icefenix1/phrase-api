10c54cf5679c065bc84d62b0252c3940ba7fefdf
Peter Bezuidenhout <icefenix@outlook.com>
2024-01-23 12:13:04 +0200

    # Setup Base Dependancies
    ## Added
    
    External Config
    External Metrics
    External Logging
    
    ## Modified
    
    n/a
    
    ## Removed
    
    n/a

--- phrase-api/Program.cs
+++ phrase-api/Program.cs
@@ -1,11 +1,33 @@
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.AzureAppConfiguration;
+using Microsoft.Extensions.Logging.ApplicationInsights;
+using Azure.Identity;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddLogging(configure => configure
+        .AddConsole()
+        .AddApplicationInsights());
+builder.Services.AddHttpClient();
+builder.Logging.AddConfiguration(builder.Configuration.GetSection("Logging"));
+builder.Configuration.AddAzureAppConfiguration(config => config
+        .Connect(Environment.GetEnvironmentVariable("APPCONFIG"))
+        .ConfigureKeyVault(kv => kv.SetCredential(new DefaultAzureCredential())));
+
+builder.Logging.AddApplicationInsights(
+        configureTelemetryConfiguration: (config) =>
+            config.ConnectionString = builder.Configuration.GetSection("insights").Value,
+            configureApplicationInsightsLoggerOptions: (options) => { }
+    );
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddApplicationInsightsTelemetry(config =>
+            config.ConnectionString = builder.Configuration.GetSection("insights").Value
+    );
 
 var app = builder.Build();
 
